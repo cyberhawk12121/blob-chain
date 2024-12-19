9e2d9aff7841e6c80f5977217c32429dbaa4ee81
Sameer Kumar <sameer@nethermind.io>
2024-12-20 01:37:23 +0530

    fix: disallowing the node to stop listening

--- cmd/node/main.go
+++ cmd/node/main.go
@@ -4,24 +4,27 @@ import (
 	"log"
 	"os"
 
-	"github.com/cyberhawk/blob-chain/node"
+	"github.com/cyberhawk/blob-chain/internal/node"
 )
 
 func main() {
+	//
 	if len(os.Args) < 3 {
 		log.Fatalf("Usage %s <local_addr> <peer1> [<peer2 ...]\n", os.Args[0])
 	}
 
-	localAddr := os.Args[1]
-	peers := os.Args[2]
+	localAddr := os.Args[1] // first one is the local address, this node is running on and listening
+	peers := os.Args[2:]    // All the rest are the peers
 
 	n, err := node.NewNode(localAddr, peers)
 	if err != nil {
 		log.Fatalf("Failed to create node: %v", err)
 	}
 
-	if err := n.Run(); err != nul {
+	if err := n.Run(); err != nil {
 		log.Fatalf("Node encountered an error: %v", err)
 	}
 
+	// Keep the program running so it can continue listening on the port.
+	select {}
 }
