18a1b2d8c1f3f27bcb782a293340ad2a2fe0a7b2
Sameer Kumar <sameer@nethermind.io>
2024-12-19 05:30:12 +0530

    create nodes from os args and flood messages through peer list

---
+++ internal/nodes/node.go
@@ -0,0 +1,24 @@
+package node
+
+type Node struct {
+	repo      Repository
+	service   Service
+	transport Transport
+}
+
+func NewNode(localAddr string, peers []string) (*Node, error) {
+	repo := NewInMemoryRepository(peers)
+	transportStub := &udpTransport{localAddr: localAddr}
+	service := NewService(repo, transportStub)
+	transport := NewUDPTransport(localAddr, service)
+
+	transportStub.service = service
+
+	n := &Node{
+		repo:      repo,
+		service:   service,
+		transport: transport,
+	}
+
+	return n, nil
+}
